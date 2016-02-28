182dab1fa1ddc812dd09ed0a3e1eb2923248f95c
MukundPradhan <replyme.mmp@gmail.com>
2016-02-28 17:58:34 +0530

    Initial commit

---
+++ app/src/main/java/com/assignments/acadgild/session2assignment1/MainActivity.java
@@ -0,0 +1,20 @@
+/*
+***********------  Session - 2  --> Assignment 1  --------  ******************
+*
+This is the program for change the launcher icon & the app name.
+
+ */
+
+package com.assignments.acadgild.session2assignment1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
